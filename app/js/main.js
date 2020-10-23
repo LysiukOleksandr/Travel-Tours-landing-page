e34592e4823bd24c35f45ef29bc928da36966163
unknown <olysiuk01@gmail.com>
2020-10-23 16:39:55 +0300

    responsive header-top

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+function menuBurger(){
+  let icon = document.querySelector('.header__nav-burger');
+  let menu = document.querySelector('.header__nav-menu')
+  icon.onclick = function(){
+    this.classList.toggle('header__nav-burger--active')
+    menu.classList.toggle('header__nav-menu--open')
+  }
+}
+
+menuBurger()
