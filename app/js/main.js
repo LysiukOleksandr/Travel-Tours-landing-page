1ccff2369fc3e4033e5ce2969ec1ef3e934dc69a
unknown <olysiuk01@gmail.com>
2020-10-24 14:15:56 +0300

    added animate CSS and wow js library

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,10 @@
-function menuBurger(){
-  let icon = document.querySelector('.header__nav-burger');
+new WOW().init()
+//
+
+function menuBurger() {
+  let icon = document.querySelector('.header__nav-burger')
   let menu = document.querySelector('.header__nav-menu')
-  icon.onclick = function(){
+  icon.onclick = function () {
     this.classList.toggle('header__nav-burger--active')
     menu.classList.toggle('header__nav-menu--open')
   }
@@ -9,36 +12,34 @@ function menuBurger(){
 
 menuBurger()
 
-
-function testimonialsSlider(){
-  
-  let sliderImages = document.querySelectorAll(".testimonials__slider-img");
-  let count = 0;
+function testimonialsSlider() {
+  let sliderImages = document.querySelectorAll('.testimonials__slider-img')
+  let count = 0
 
   function slider() {
     for (let i = 0; i < sliderImages.length; i++) {
-      sliderImages[i].classList.add("testimonials__slider-img--hidden");
+      sliderImages[i].classList.add('testimonials__slider-img--hidden')
     }
-    sliderImages[count].classList.remove("testimonials__slider-img--hidden");
+    sliderImages[count].classList.remove('testimonials__slider-img--hidden')
   }
 
-  document.querySelector(".testimonials__slider-prev").onclick = function () {
+  document.querySelector('.testimonials__slider-prev').onclick = function () {
     if (count - 1 == -1) {
-      count = sliderImages.length - 1;
+      count = sliderImages.length - 1
     } else {
-      count--;
+      count--
     }
-    slider();
-  };
+    slider()
+  }
 
-  document.querySelector(".testimonials__slider-next").onclick = function () {
+  document.querySelector('.testimonials__slider-next').onclick = function () {
     if (count + 1 == sliderImages.length) {
-      count = 0;
+      count = 0
     } else {
-      count++;
+      count++
     }
-    slider();
-  };
+    slider()
+  }
 }
 
 testimonialsSlider()
